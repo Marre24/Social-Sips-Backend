3bc09bc7ec8aba8c5bce517c824214d1fb95c81f
Marre24 <93831034+Marre24@users.noreply.github.com>
2025-04-15 13:52:14 +0200

    init commit

---
+++ src/main/java/com/pvt/SocialSips/SocialSipsApplication.java
@@ -0,0 +1,13 @@
+package com.pvt.SocialSips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pvt/SocialSips/SocialSipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvt.SocialSips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
