621ffe20b1fc0709e9481167418455bb99e813e2
Albin Phillips <albin.bryn@gmail.com>
2025-04-16 14:26:53 +0200

    [ADD] hello world method

--- src/main/java/com/pvt/SocialSips/SocialSipsApplication.java
+++ src/main/java/com/pvt/SocialSips/SocialSipsApplication.java
@@ -2,12 +2,19 @@ package com.pvt.SocialSips;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SocialSipsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialSipsApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return "Hello world!";
+	}
 }
