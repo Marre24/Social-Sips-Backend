617f4cb96e0f224638ad090f77378879a05b7461
serhatDSV <seak4365@student.su.se>
2025-04-24 10:45:09 +0200

    [ADD] instance variables to Questpool

--- src/main/java/com/pvt/SocialSips/questpool/Questpool.java
+++ src/main/java/com/pvt/SocialSips/questpool/Questpool.java
@@ -1,5 +1,19 @@
 package com.pvt.SocialSips.questpool;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Questpool {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String category;
+
+
 
 }
