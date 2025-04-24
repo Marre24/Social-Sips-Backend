f4e9be680192228a0d6af78fc781938891a5e732
serhatDSV <seak4365@student.su.se>
2025-04-24 11:16:29 +0200

    [ADD] get by userID endpoint

--- src/main/java/com/pvt/SocialSips/questpool/QuestpoolController.java
+++ src/main/java/com/pvt/SocialSips/questpool/QuestpoolController.java
@@ -1,8 +1,15 @@
 package com.pvt.SocialSips.questpool;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/questpool")
 public class QuestpoolController {
+
+    @GetMapping("/{userId}")
+    public String getByUserId(@PathVariable String userId) {
+        return userId;
+    }
+
 
 }
