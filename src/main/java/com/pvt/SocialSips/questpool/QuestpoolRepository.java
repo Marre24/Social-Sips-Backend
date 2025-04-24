ea22d6a7692e812ed885be9cfb57adc3dbf42dbc
serhatDSV <seak4365@student.su.se>
2025-04-24 10:48:27 +0200

    [ADD] QuestpoolRepository

---
+++ src/main/java/com/pvt/SocialSips/questpool/QuestpoolRepository.java
@@ -0,0 +1,7 @@
+package com.pvt.SocialSips.questpool;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestpoolRepository extends JpaRepository<Questpool, Integer> {
+
+}
