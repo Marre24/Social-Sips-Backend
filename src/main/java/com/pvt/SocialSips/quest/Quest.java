1b5c97dc21b247e9f803a56c51a7850fe901efbd
Nomis778 <sijulemi@gmail.com>
2025-04-24 11:08:29 +0200

    [ADD] sequenced id

--- src/main/java/com/pvt/SocialSips/quest/Quest.java
+++ src/main/java/com/pvt/SocialSips/quest/Quest.java
@@ -1,4 +1,17 @@
 package com.pvt.SocialSips.quest;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.id.IncrementGenerator;
+
+@Data
+@Entity
+@Table
 public class Quest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long questId;
+
+    private String prompt;
+    private String type;
 }
