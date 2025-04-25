910dcad2eb2cc3975493dc4e2fb87be297184446
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-04-25 15:11:02 +0200

    [ADD] delete questpool

--- src/main/java/com/pvt/SocialSips/questpool/QuestpoolService.java
+++ src/main/java/com/pvt/SocialSips/questpool/QuestpoolService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -13,7 +14,6 @@ public class QuestpoolService {
 
     @Autowired
     private QuestpoolRepository questpoolRepository;
-
 
     public QuestpoolService(QuestpoolRepository questpoolRepository) {
         this.questpoolRepository = questpoolRepository;
@@ -47,5 +47,10 @@ public class QuestpoolService {
         }
 
 
+    }
+
+    public void deleteQuestpool(Questpool qp) {
+        getByQuestpoolId(qp.getId());
+        questpoolRepository.delete(qp);
     }
 }
