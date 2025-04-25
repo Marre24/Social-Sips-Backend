4a75d8d9c2bb6561b49cac1897f511c3be2b0919
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-04-25 13:41:10 +0200

    [DELETE] QuestRepository.java and QuestService.java

--- src/main/java/com/pvt/SocialSips/questpool/QuestpoolController.java
+++ src/main/java/com/pvt/SocialSips/questpool/QuestpoolController.java
@@ -2,7 +2,6 @@ package com.pvt.SocialSips.questpool;
 
 
 import com.pvt.SocialSips.quest.Quest;
-import com.pvt.SocialSips.quest.QuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +10,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/questpool")
 public class QuestpoolController {
 
-    @Autowired
-    private final QuestService questService;
-
-    public QuestpoolController(QuestService questService) {
-        this.questService = questService;
-    }
 
     @GetMapping("/{userId}")
     public Long getByUserId(@PathVariable Long userId) {
@@ -26,17 +19,5 @@ public class QuestpoolController {
     @PostMapping("/{userId}")
     public HttpStatus addQuestpool(@PathVariable Long userId) {
         return HttpStatus.ACCEPTED;
-    }
-
-    @PostMapping("/")
-    public void createQuest(@RequestBody Quest quest, @RequestParam String type){
-        questService.createQuest(quest);
-    }
-
-    @PatchMapping("/edit/{id}")
-    public void editQuest(@RequestBody Quest quest, @PathVariable Long id){
-
-        System.out.println(quest);
-        questService.editQuest(quest, id);
     }
 }
