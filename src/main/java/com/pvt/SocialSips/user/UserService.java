bded4dbaf8d096f1b72806db1a310bfa5547fa81
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-04-28 15:28:22 +0200

    [ADD] UserService corresponding controller methods

--- src/main/java/com/pvt/SocialSips/user/UserService.java
+++ src/main/java/com/pvt/SocialSips/user/UserService.java
@@ -1,7 +1,32 @@
 package com.pvt.SocialSips.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User login(@Validated User user) {
+        Optional<User> u = userRepository.findById(user.getId());
+        if(authenticate(user)) return u.orElseThrow(() -> new IllegalStateException("Not a valid user!"));
+        return null;
+    }
+
+    public boolean authenticate(User user){
+        return true;
+    }
+
+    public void register(User user) {
+
+    }
 }
