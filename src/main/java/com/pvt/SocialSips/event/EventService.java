ee2cfbbf4bf8a6ac859d62d15de6788377dce708
Marre24 <maximilian.ellnestam@gmail.com>
2025-04-28 12:54:09 +0200

    [ADD] createEvent test

--- src/main/java/com/pvt/SocialSips/event/EventService.java
+++ src/main/java/com/pvt/SocialSips/event/EventService.java
@@ -1,7 +1,20 @@
 package com.pvt.SocialSips.event;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public void createEvent(Event event) {
+        eventRepository.save(event);
+    }
+
 }
--- src/test/java/com/pvt/SocialSips/event/EventServiceTest.java
+++ src/test/java/com/pvt/SocialSips/event/EventServiceTest.java
@@ -1,4 +1,35 @@
 package com.pvt.SocialSips.event;
 
+import com.pvt.SocialSips.questpool.Questpool;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 public class EventServiceTest {
+
+    @Mock
+    private EventRepository eventRepository;
+
+    @InjectMocks
+    private EventService eventService;
+
+    private static Event event = new Event(1L, "name", 2, new HashSet<>());
+
+
+    @Test
+    public void createEvent_EmptyDatabase_EventCreated(){
+        when(eventRepository.save(event)).thenReturn(event);
+
+        assertDoesNotThrow(() -> eventService.createEvent(event));
+    }
+
 }
