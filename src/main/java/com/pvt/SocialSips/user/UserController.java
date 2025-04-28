7bdc99853f5ab93b8e979fc847bde46b9518242c
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-04-28 14:42:45 +0200

    [ADD] UserService and UserController

---
+++ src/main/java/com/pvt/SocialSips/user/UserController.java
@@ -0,0 +1,23 @@
+package com.pvt.SocialSips.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/login")
+    public ResponseEntity<?> login(User user){
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/pvt/SocialSips/user/UserService.java
@@ -0,0 +1,7 @@
+package com.pvt.SocialSips.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
