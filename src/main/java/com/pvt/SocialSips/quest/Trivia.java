d9481a516f2aa5f96d106955162d19836c183de1
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-04-28 15:47:33 +0200

    [ADD] Getter and setter for Trivia

--- src/main/java/com/pvt/SocialSips/quest/Trivia.java
+++ src/main/java/com/pvt/SocialSips/quest/Trivia.java
@@ -1,10 +1,12 @@
 package com.pvt.SocialSips.quest;
 
 
+import java.util.Arrays;
+
 public class Trivia extends Quest {
 
-    private final String[] options;
-    private final int correctOption;
+    private String[] options;
+    private int correctOption;
 
     public Trivia(Long id, String prompt, String[] options, int correctOption){
         super(id, prompt);
@@ -12,8 +14,16 @@ public class Trivia extends Quest {
         this.options = options;
     }
 
+    public void setOptions(String[] options){;
+        this.options = options;
+    }
+
     public String[] getOptions() {
         return options;
+    }
+
+    public void setCorrectOption(int correctOption){
+        this.correctOption = correctOption;
     }
 
     public String getCorrectOption() {
