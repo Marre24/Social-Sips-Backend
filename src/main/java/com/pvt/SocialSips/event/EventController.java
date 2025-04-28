b1e776a3c7f2ad348f2086c20b204ed191b3368c
Marre24 <maximilian.ellnestam@gmail.com>
2025-04-28 14:27:10 +0200

    [ADD] getEvent

--- src/main/java/com/pvt/SocialSips/event/EventController.java
+++ src/main/java/com/pvt/SocialSips/event/EventController.java
@@ -1,10 +1,32 @@
 package com.pvt.SocialSips.event;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/event")
 public class EventController {
 
+    @Autowired
+    private EventService eventService;
 
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/{hostId}")
+    public ResponseEntity<?> getEvent(@PathVariable Long hostId){
+        try{
+            Event e = eventService.getEvent(hostId);
+            return new ResponseEntity<>(e, HttpStatus.OK);
+
+        } catch (EntityNotFoundException exception){
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
 
 }
