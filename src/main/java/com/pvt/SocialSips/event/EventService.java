78a05d17d62fe6fa570764bf38dd2768cd530f3e
Marre24 <maximilian.ellnestam@gmail.com>
2025-04-28 15:21:47 +0200

    [ADD] transactional

--- src/main/java/com/pvt/SocialSips/event/EventService.java
+++ src/main/java/com/pvt/SocialSips/event/EventService.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -34,6 +35,7 @@ public class EventService {
     }
 
 
+    @Transactional
     public void startEvent(Long hostId) {
         Event event = getEvent(hostId);
 
