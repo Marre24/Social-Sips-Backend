97ad1073b516a53ab94cb67c0d0f666f8b3c6cbf
maxi <maxi.ellnestam@gmail.com>
2025-05-07 15:54:41 +0200

    [ADD] csrf to tests

--- src/test/java/com/pvt/SocialSips/user/UserIntegrationTest.java
+++ src/test/java/com/pvt/SocialSips/user/UserIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.pvt.SocialSips.user;
 
+import com.pvt.SocialSips.role.Role;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -9,18 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 
@@ -31,19 +31,16 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserIntegrationTest {
 
+    private static final String URL = "https://localhost/";
+
     private static final String TEST_USER_SUB = "TEST USER SUB";
 
-    private static final String URL = "https://localhost/user";
-
-
     private static final OidcUser OIDC_USER = new DefaultOidcUser(
-            AuthorityUtils.createAuthorityList("ROLE_HOST"),
+            AuthorityUtils.createAuthorityList("ROLE_OIDC_USER"),
             OidcIdToken.withTokenValue("id-token").claim("sub", TEST_USER_SUB).build(),
             "sub");
 
-    private static final int ZERO = 0;
-
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     private final MockMvc mockMvc;
 
@@ -54,18 +51,20 @@ public class UserIntegrationTest {
     }
 
     @BeforeAll
-    public void setup(){
-        userRepository.save(new User(TEST_USER_SUB, TEST_USER_SUB));
+    public void setup() {
+        userRepository.save(new User(TEST_USER_SUB, TEST_USER_SUB, List.of(new Role("ROLE_OIDC_USER"))));
     }
 
     @AfterAll
-    public void shutDown(){
-        userRepository.delete(new User(TEST_USER_SUB, TEST_USER_SUB));
+    public void shutDown() {
+        userRepository.delete(new User(TEST_USER_SUB, TEST_USER_SUB, List.of(new Role("ROLE_OIDC_USER"))));
     }
 
     @Test
     public void getAllQuestpools_HostExists_HTTPCodeIsOk() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(URL + "/").with(oidcLogin().oidcUser(OIDC_USER)))
+        mockMvc.perform(MockMvcRequestBuilders.get(URL + "user/")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .with(oidcLogin().oidcUser(OIDC_USER)))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
