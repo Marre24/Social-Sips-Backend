fb7ba28cd3056fbb4a94ce4f8ff4464174dd0d99
maxi <maxi.ellnestam@gmail.com>
2025-05-07 12:28:10 +0200

    [ADD] getAllQuestpools for user integration test

--- src/test/java/com/pvt/SocialSips/user/UserIntegrationTest.java
+++ src/test/java/com/pvt/SocialSips/user/UserIntegrationTest.java
@@ -1,7 +1,52 @@
 package com.pvt.SocialSips.user;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
 public class UserIntegrationTest {
+
+
+    private static final OidcUser OIDC_USER = new DefaultOidcUser(
+            AuthorityUtils.createAuthorityList("ROLE_HOST"),
+            OidcIdToken.withTokenValue("id-token").claim("sub", "STANDARD").build(),
+            "sub");
+
+    private static final int ZERO = 0;
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    public UserIntegrationTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void getAllQuestpools_HostExists_HTTPCodeIsOk() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("https://localhost/user/").with(oidcLogin().oidcUser(OIDC_USER)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+
 }
