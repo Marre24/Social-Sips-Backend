76c4846aa34180999ab9b7c95b99bb2778292027
maxi <maxi.ellnestam@gmail.com>
2025-05-07 13:35:16 +0200

    [ADD] a temporary user to tests

--- src/test/java/com/pvt/SocialSips/user/UserIntegrationTest.java
+++ src/test/java/com/pvt/SocialSips/user/UserIntegrationTest.java
@@ -1,6 +1,9 @@
 package com.pvt.SocialSips.user;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +16,7 @@ import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -24,26 +28,44 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserIntegrationTest {
+
+    private static final String TEST_USER_SUB = "TEST USER SUB";
+
+    private static final String URL = "https://localhost/user";
 
 
     private static final OidcUser OIDC_USER = new DefaultOidcUser(
             AuthorityUtils.createAuthorityList("ROLE_HOST"),
-            OidcIdToken.withTokenValue("id-token").claim("sub", "STANDARD").build(),
+            OidcIdToken.withTokenValue("id-token").claim("sub", TEST_USER_SUB).build(),
             "sub");
 
     private static final int ZERO = 0;
 
-    private MockMvc mockMvc;
+    private UserRepository userRepository;
+
+    private final MockMvc mockMvc;
 
     @Autowired
-    public UserIntegrationTest(MockMvc mockMvc) {
+    public UserIntegrationTest(UserRepository userRepository, MockMvc mockMvc) {
+        this.userRepository = userRepository;
         this.mockMvc = mockMvc;
+    }
+
+    @BeforeAll
+    public void setup(){
+        userRepository.save(new User(TEST_USER_SUB, TEST_USER_SUB));
+    }
+
+    @AfterAll
+    public void shutDown(){
+        userRepository.delete(new User(TEST_USER_SUB, TEST_USER_SUB));
     }
 
     @Test
     public void getAllQuestpools_HostExists_HTTPCodeIsOk() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("https://localhost/user/").with(oidcLogin().oidcUser(OIDC_USER)))
+        mockMvc.perform(MockMvcRequestBuilders.get(URL + "/").with(oidcLogin().oidcUser(OIDC_USER)))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
