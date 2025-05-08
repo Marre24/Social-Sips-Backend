acf435e3c0a064adafca688d6ddb92f5e6eaad5a
Nomis778 <sijulemi@gmail.com>
2025-05-08 17:52:15 +0200

    [ADD] get- and create event tests

--- src/test/java/com/pvt/SocialSips/event/EventIT.java
+++ src/test/java/com/pvt/SocialSips/event/EventIT.java
@@ -4,21 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.pvt.SocialSips.quest.Icebreaker;
-import com.pvt.SocialSips.quest.Trivia;
-import com.pvt.SocialSips.questpool.Questpool;
-import com.pvt.SocialSips.questpool.QuestpoolType;
 import com.pvt.SocialSips.role.Role;
 import com.pvt.SocialSips.user.User;
-import com.pvt.SocialSips.user.UserRepository;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import com.pvt.SocialSips.user.UserService;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
@@ -34,6 +28,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -41,25 +36,36 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class EventIT {
 
-    private static final String TEST_USER_SUB = "TEST USER SUB";
+    private static final String USER_SUB_WITH_EVENT = "731723";
 
-    private static final User USER = new User("THIS IS A FIRST NAME", TEST_USER_SUB, List.of(new Role("ROLE_OIDC_USER")));
-    private static final Questpool QUESTPOOL_ONE = new Questpool("A quest pool", QuestpoolType.ICEBREAKER, new HashSet<>(List.of(new Icebreaker("prompt"))));
-    private static final Questpool QUESTPOOL_TWO = new Questpool("A quest pool", QuestpoolType.ICEBREAKER, new HashSet<>(List.of(new Icebreaker("prompt"))));
-    private static final Questpool QUESTPOOL_THREE = new Questpool("A quest pool", QuestpoolType.TRIVIA,
-            new HashSet<>(List.of(new Trivia("Question two", new HashSet<>(List.of("opp1", "correct", "opp3", "opp4")), 2))));
+    private static final User USER_WITH_EVENT = new User("THIS IS A FIRST NAME", USER_SUB_WITH_EVENT, List.of(new Role("ROLE_OIDC_USER")));
+    private static final Event EVENT = new Event("THIS IS AN EVENT", 2, new HashSet<>(), USER_SUB_WITH_EVENT);
 
     private static final OidcUser OIDC_USER = new DefaultOidcUser(
-            AuthorityUtils.createAuthorityList("ROLE_OIDC_USER"),
-            OidcIdToken.withTokenValue("id-token").claim("sub", TEST_USER_SUB).build(),
+            AuthorityUtils.createAuthorityList("ROLE_HOST"),
+            OidcIdToken.withTokenValue("id-token").claim("sub", USER_SUB_WITH_EVENT).build(),
             "sub");
 
-    private final UserRepository userRepository;
+    private static final String USER_SUB_WITHOUT_EVENT = "696969";
+    private static final User USER_WITHOUT_EVENT = new User("THIS IS A FIRST NAME", USER_SUB_WITHOUT_EVENT, List.of(new Role("ROLE_OIDC_USER")));
+    private static final Event EVENT_WITHOUT = new Event("THIS IS AN EVENT", 2, new HashSet<>(), USER_SUB_WITHOUT_EVENT);
+
+    private static final OidcUser OIDC_USER_WITHOUT_EVENTS = new DefaultOidcUser(
+            AuthorityUtils.createAuthorityList("ROLE_HOST"),
+            OidcIdToken.withTokenValue("id-token").claim("sub", USER_SUB_WITHOUT_EVENT).build(),
+            "sub");
+
+    private static String EVENT_WITHOUT_IN_JSON_EXPECTED;
+    private static String EVENT_IN_JSON_EXPECTED;
+
+    private final UserService userService;
+    private final EventService eventService;
     private final MockMvc mockMvc;
 
     @Autowired
-    public EventIT(UserRepository userRepository, MockMvc mockMvc){
-        this.userRepository = userRepository;
+    public EventIT(UserService userService, EventService eventService, MockMvc mockMvc){
+        this.userService = userService;
+        this.eventService = eventService;
         this.mockMvc = mockMvc;
     }
 
@@ -69,24 +75,65 @@ public class EventIT {
         mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
 
-        USER.addQuestpool(QUESTPOOL_ONE);
-        USER.addQuestpool(QUESTPOOL_TWO);
-        USER.addQuestpool(QUESTPOOL_THREE);
-        userRepository.save(USER);
+        userService.register(USER_WITH_EVENT);
+        eventService.createEvent(EVENT, USER_SUB_WITH_EVENT);
+
+        userService.register(USER_WITHOUT_EVENT);
+
+        EVENT_IN_JSON_EXPECTED = ow.writeValueAsString(EVENT);
+        EVENT_WITHOUT_IN_JSON_EXPECTED = ow.writeValueAsString(EVENT_WITHOUT);
+    }
+
+
+
+    @BeforeEach
+    public void beforeEach(){
+        userService.deleteUser(USER_WITHOUT_EVENT);
+        userService.register(USER_WITHOUT_EVENT);
     }
 
     @AfterAll
     public void shutDown() {
-        userRepository.deleteById(USER.getSub());
+        userService.deleteUser(USER_WITH_EVENT);
+        userService.deleteUser(USER_WITHOUT_EVENT);
     }
 
     @Test
-    public void getEvent(){
-//        mockMvc.perform(MockMvcRequestBuilders.get("/event/").secure(true)
-//                        .with(SecurityMockMvcRequestPostProcessors.csrf())
-//                        .with(oidcLogin().oidcUser(OIDC_USER)))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(MockMvcResultMatchers.status().isOk());
+    public void getEvent_HostWithEvent_EventReturned() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/event/").secure(true)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .with(oidcLogin().oidcUser(OIDC_USER)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().json(EVENT_IN_JSON_EXPECTED));
+    }
+
+    @Test
+    public void createEvent_HostWithoutEvent_HTTPCodeOk() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/event/").secure(true)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .with(oidcLogin().oidcUser(OIDC_USER_WITHOUT_EVENTS))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(EVENT_IN_JSON_EXPECTED))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void createEvent_HostWithoutEvent_EventCreated() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/event/").secure(true)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .with(oidcLogin().oidcUser(OIDC_USER_WITHOUT_EVENTS))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(EVENT_WITHOUT_IN_JSON_EXPECTED))
+                .andDo(MockMvcResultHandlers.print());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/event/").secure(true)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .with(oidcLogin().oidcUser(OIDC_USER_WITHOUT_EVENTS)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().json(EVENT_WITHOUT_IN_JSON_EXPECTED));
     }
 
 }
