c06f506d4d54def1fac82f61beb571331c1f7941
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-05-08 13:43:53 +0200

    [ADD] FirebaseConfig for OAuth2

---
+++ src/main/java/com/pvt/SocialSips/config/FirebaseConfig.java
@@ -0,0 +1,36 @@
+package com.pvt.SocialSips.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value(value = "${FIREBASE_ADMIN_SDK}")
+    private Resource serviceAccountResource;
+
+    @PostConstruct
+    public void initialize() {
+        try (InputStream serviceAccount = serviceAccountResource.getInputStream()){
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
