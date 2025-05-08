60219b6190db32c56f2442de98295fac80502c2f
maxi <maxi.ellnestam@gmail.com>
2025-05-08 15:06:17 +0200

    [ADD] deletion tests

--- src/test/java/com/pvt/SocialSips/questpool/QuestpoolIT.java
+++ src/test/java/com/pvt/SocialSips/questpool/QuestpoolIT.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.pvt.SocialSips.quest.Icebreaker;
+import com.pvt.SocialSips.quest.Trivia;
 import com.pvt.SocialSips.role.Role;
 import com.pvt.SocialSips.user.User;
 import com.pvt.SocialSips.user.UserService;
@@ -31,6 +32,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 
@@ -48,7 +50,11 @@ public class QuestpoolIT {
             OidcIdToken.withTokenValue("id-token").claim("sub", TEST_USER_SUB).build(),
             "sub");
 
-    private static final Questpool QUESTPOOL = new Questpool("A quest pool", QuestpoolType.ICEBREAKER, new HashSet<>(List.of(new Icebreaker("prompt"))));
+    private static final Questpool QUESTPOOL_TO_BE_ADDED = new Questpool("A quest pool", QuestpoolType.ICEBREAKER, new HashSet<>(List.of(new Icebreaker("prompt"))));
+    private static final Questpool QUESTPOOL_ONE = new Questpool("A quest pool", QuestpoolType.ICEBREAKER, new HashSet<>(List.of(new Icebreaker("prompt"))));
+    private static final Questpool QUESTPOOL_TWO = new Questpool("A quest pool", QuestpoolType.ICEBREAKER, new HashSet<>(List.of(new Icebreaker("prompt"))));
+    private static final Questpool QUESTPOOL_THREE = new Questpool("A quest pool", QuestpoolType.TRIVIA,
+            new HashSet<>(List.of(new Trivia("Question two", new HashSet<>(List.of("opp1", "correct", "opp3", "opp4")), 2))));
 
     private final UserService userService;
 
@@ -61,7 +67,15 @@ public class QuestpoolIT {
     }
 
     @BeforeAll
-    public void setup() throws Exception {
+    public void setup() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+
+        TEST_USER.addQuestpool(QUESTPOOL_ONE);
+        TEST_USER.addQuestpool(QUESTPOOL_TWO);
+        TEST_USER.addQuestpool(QUESTPOOL_THREE);
+
         userService.register(TEST_USER);
     }
 
@@ -75,7 +89,7 @@ public class QuestpoolIT {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-        String requestJson = ow.writeValueAsString(QUESTPOOL);
+        String requestJson = ow.writeValueAsString(QUESTPOOL_TO_BE_ADDED);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/questpool/")
                         .with(oidcLogin().oidcUser(OIDC_USER)).secure(true)
@@ -91,7 +105,7 @@ public class QuestpoolIT {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-        String requestJson = ow.writeValueAsString(QUESTPOOL);
+        String requestJson = ow.writeValueAsString(QUESTPOOL_TO_BE_ADDED);
 
         int expectedAmount = userService.getUserBySub(TEST_USER_SUB).getQuestpools().size() + 1;
 
@@ -100,6 +114,42 @@ public class QuestpoolIT {
                         .with(SecurityMockMvcRequestPostProcessors.csrf())
                         .contentType(APPLICATION_JSON)
                         .content(requestJson))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        int actual = userService.getUserBySub(TEST_USER_SUB).getQuestpools().size();
+        assertEquals(expectedAmount, actual);
+    }
+
+    @Test
+    public void deleteQuestpool_ValidHost_HTTPCodeIsOK() throws Exception {
+        var set = userService.getUserBySub(TEST_USER_SUB).getQuestpools();
+
+        assertFalse(set.isEmpty());
+
+        var it = set.iterator();
+        var questpool = it.next();
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/questpool/" + questpool.getId())
+                        .with(oidcLogin().oidcUser(OIDC_USER)).secure(true)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void deleteQuestpool_ValidHost_HostQuestpoolsSizeIsDecrementedByOne() throws Exception {
+        var set = userService.getUserBySub(TEST_USER_SUB).getQuestpools();
+
+        assertFalse(set.isEmpty());
+
+        int expectedAmount = set.size() - 1;
+        var it = set.iterator();
+        var questpool = it.next();
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/questpool/" + questpool.getId())
+                        .with(oidcLogin().oidcUser(OIDC_USER)).secure(true)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
