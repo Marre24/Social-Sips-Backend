5e4e0f5590abeac4071399e978cb730e8f6792d0
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-05-16 19:15:58 +0200

    [UPDATE]

--- src/main/java/com/pvt/SocialSips/user/FirebaseUserController.java
+++ src/main/java/com/pvt/SocialSips/user/FirebaseUserController.java
@@ -1,7 +1,9 @@
 package com.pvt.SocialSips.user;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import com.pvt.SocialSips.role.Role;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,25 +26,27 @@ import java.util.stream.Collectors;
 @RequestMapping("/auth")
 public class FirebaseUserController {
 
-    private final OidcUserDetailsService oidcUserDetailsService;
+    private final UserService userService;
 
     @Autowired
-    public FirebaseUserController(OidcUserDetailsService oidcUserDetailsService) {
-        this.oidcUserDetailsService = oidcUserDetailsService;
+    public FirebaseUserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/token")
-    public ResponseEntity<?> authenticateFirebaseToken(OidcUserRequest oidcUserRequest, @RequestBody FirebaseToken firebaseToken) {
+    public ResponseEntity<?> authenticateFirebaseToken(GoogleCredentials credentials, @RequestBody FirebaseToken firebaseToken) throws FirebaseAuthException {
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(firebaseToken.toString());
 
-        OidcUser user = oidcUserDetailsService.loadUser(oidcUserRequest);
-//        if (user == null) {
-//
-//            user = userService.register(new User(firebaseToken.getName(), firebaseToken.getUid(), List.of(new Role("OIDC_USER"), new Role("HOST"))));
-//            List<GrantedAuthority> authorities = user.getRoles().stream()
-//                    .map(role -> new SimpleGrantedAuthority(role.getName()))
-//                    .collect(Collectors.toList());
-//        }
+        List<Role> roles = List.of(new Role("HOST"), new Role("OIDC_USER"));
+                PreAuthenticatedAuthenticationToken authentication = new PreAuthenticatedAuthenticationToken(
+                decodedToken.getEmail(), null, Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        if(userService.getUserBySub(firebaseToken.getUid()) == null){
+            User user = new User(firebaseToken.getName(),firebaseToken.getUid(), roles);
+            userService.register(user);
+        }
 
-        return ResponseEntity.status(HttpStatus.OK).body(user);
+
+        return ResponseEntity.status(HttpStatus.OK).body("User authenticated.");
     }
 }
