6fcb17562a7694e91d96a4edac3cd9f30215ff2d
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-05-16 19:00:08 +0200

    [UPDATE]

--- src/main/java/com/pvt/SocialSips/config/FirebaseConfig.java
+++ src/main/java/com/pvt/SocialSips/config/FirebaseConfig.java
@@ -3,7 +3,9 @@ package com.pvt.SocialSips.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
@@ -30,6 +32,11 @@ public class FirebaseConfig {
         } catch (IOException e) {
             System.out.println(e.getCause() + e.getMessage());
         }
+    }
+
+    @Bean
+    public FirebaseAuth firebaseAuth() {
+        return FirebaseAuth.getInstance(FirebaseApp.getInstance());
     }
 
 }
