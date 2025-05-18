be278242ff6684b95cb50577b72c3c9c988788ab
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-05-18 14:21:02 +0200

    [ADD] constant class for readability

---
+++ src/main/java/com/pvt/SocialSips/constants/WebConstants.java
@@ -0,0 +1,19 @@
+package com.pvt.SocialSips.constants;
+
+public final class WebConstants {
+        public static final String[] PROTECTED_ENDPOINTS = {"/user/**", "/event/", "/event/start/", "/questpool/**"};
+
+        public static final String[] PERMITTED_ENDPOINTS = {"/", "/home", "/css/**", "/error", "/__/hosting/**",
+                                                            "/event/join/**", "/auth/token", "/.well-known/**",
+                                                            "/h2-console/**"};
+
+        public static final String[] ALLOWED_ORIGINS = {"https://social-sips-ec954.web.app",
+                                                            "https://social-sips-ec954.firebaseapp.com",
+                                                            "ws://127.0.0.1:5000/"};
+
+        public static final String AUTHORIZATION_HEADER = "Authorization";
+
+        public static final String BEARER_PREFIX = "Bearer ";
+
+        public static final String USER_ID_CLAIM = "user_id";
+}
