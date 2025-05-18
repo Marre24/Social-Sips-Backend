6a87759ef18a82bafac846a00bc12ad087dce6de
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-05-18 14:19:24 +0200

    [UPDATE] cleaned up the filterChain flow and CORS/CSRF and oauth2 filter handling

--- src/main/java/com/pvt/SocialSips/config/SecurityConfig.java
+++ src/main/java/com/pvt/SocialSips/config/SecurityConfig.java
@@ -12,56 +12,52 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static com.pvt.SocialSips.constants.WebConstants.*;
+
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig implements WebMvcConfigurer {
 
-    private final String[] PROTECTED_ENDPOINTS = {"/user/**", "/event/", "/event/start/", "/questpool/**"};
     private final OidcUserDetailsService oidcUserDetailsService;
-    private final AuthenticationSuccessHandlerConfig successHandlerConfig;
-
+    private final FirebaseAuthenticationFilter firebaseAuthenticationFilter;
     @Autowired
-    public SecurityConfig(OidcUserDetailsService oidcUserDetailsService, AuthenticationSuccessHandlerConfig successHandlerConfig, UserService userService) {
+    public SecurityConfig(OidcUserDetailsService oidcUserDetailsService, AuthenticationSuccessHandlerConfig successHandlerConfig, UserService userService, FirebaseAuthenticationFilter firebaseAuthenticationFilter) {
         this.oidcUserDetailsService = oidcUserDetailsService;
-        this.successHandlerConfig = successHandlerConfig;
+        this.firebaseAuthenticationFilter = firebaseAuthenticationFilter;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+                .securityMatcher("/**")
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers( "/", "/home", "/css/**", "/error", "/__/hosting/**").permitAll()
-                        .requestMatchers("/event/join/**", "/auth/token", "/.well-known/**").permitAll()
-                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
-                        .requestMatchers(HttpMethod.PUT).permitAll()
-                        .requestMatchers(HttpMethod.POST).permitAll()
-                        .requestMatchers(HttpMethod.PATCH).permitAll()
-                        .requestMatchers(HttpMethod.DELETE).permitAll()
-                        .requestMatchers(HttpMethod.GET).permitAll()
-                        .requestMatchers(PROTECTED_ENDPOINTS    ).authenticated()
-                )
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/user/**", "/event/**", "/questpool/**", "/ws/**", "/auth/token")
+                        .requestMatchers(HttpMethod.POST, PERMITTED_ENDPOINTS).permitAll()
+                        .requestMatchers(HttpMethod.GET, PERMITTED_ENDPOINTS).permitAll()
+                        .requestMatchers(PROTECTED_ENDPOINTS).authenticated()
                 )
                 .oauth2Login(cfg -> cfg
-                        .successHandler(authenticationSuccessHandler())
                         .userInfoEndpoint(custom -> custom.oidcUserService(oidcUserDetailsService)))
-                .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(firebaseAuthenticationFilter, BasicAuthenticationFilter.class)
                 .build();
     }
 
@@ -71,10 +67,7 @@ public class SecurityConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins(
-                                    "https://social-sips-ec954.web.app",
-                                    "https://social-sips-ec954.firebaseapp.com"
-                                    , "ws://127.0.0.1:5000/")
+                            .allowedOrigins(ALLOWED_ORIGINS)
                             .allowedMethods("*")
                             .allowedHeaders("*")
                             .allowCredentials(true);
@@ -88,19 +81,12 @@ public class SecurityConfig implements WebMvcConfigurer {
         configurer.setUseTrailingSlashMatch(true);
     }
 
-
-
     @Bean
     FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
         final FilterRegistrationBean<ForwardedHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new ForwardedHeaderFilter());
         filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return filterRegistrationBean;
-    }
-
-    @Bean
-    public AuthenticationSuccessHandler authenticationSuccessHandler() {
-        return successHandlerConfig;
     }
 
     @Bean
