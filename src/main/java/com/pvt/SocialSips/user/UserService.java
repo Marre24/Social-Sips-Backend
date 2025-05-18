452b7a27691d8e4af9e0ff38f03f3b30c4eaa4b0
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-05-18 14:20:19 +0200

    [UPDATE] incoprprated getting authenticated user details from SecurityContextHolder based on new Firebase auth impl

--- src/main/java/com/pvt/SocialSips/user/UserService.java
+++ src/main/java/com/pvt/SocialSips/user/UserService.java
@@ -1,14 +1,18 @@
 package com.pvt.SocialSips.user;
 
+import com.google.firebase.auth.FirebaseToken;
 import com.pvt.SocialSips.questpool.Questpool;
+import com.pvt.SocialSips.role.Role;
+import com.pvt.SocialSips.token.FirebaseAuthenticationToken;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -64,5 +68,25 @@ public class UserService {
     @Transactional
     public void removeQuestpoolFrom(User user, Questpool qp) {
         user.removeQuestpool(qp);
+    }
+
+    public User getOrCreateUser(String userUid) {
+        User u = userRepository.findById(userUid).orElse(null);
+        if(u == null){
+            Object o = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if(o instanceof FirebaseAuthenticationToken token){
+                FirebaseToken firebaseToken = token.getFirebaseToken();
+                u = new User(firebaseToken.getName(),
+                        firebaseToken.getUid(),
+                        token.getAuthorities().stream()
+                                .map(auth -> new Role(auth
+                                        .getAuthority()))
+                                .collect(Collectors.toList()));
+                userRepository.save(u);
+            }
+            else System.out.println(o);
+        }
+        return u;
+
     }
 }
