63e3f188bb9708999d13680208d85318b836f2ea
maxi <maxi.ellnestam@gmail.com>
2025-05-21 13:44:53 +0200

    [UPDATE] mocking of it user test to use jwt

--- src/main/java/com/pvt/SocialSips/util/JwtParser.java
+++ src/main/java/com/pvt/SocialSips/util/JwtParser.java
@@ -2,12 +2,14 @@ package com.pvt.SocialSips.util;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
 
 public final class JwtParser {
 
+
     public static String extractSub(){
         Jwt jwt = getJwtFromSecurityContextHolder();
-        return jwt.getClaim("sub");
+        return jwt.getClaim(JwtClaimNames.SUB);
     }
 
     private static Jwt getJwtFromSecurityContextHolder(){
