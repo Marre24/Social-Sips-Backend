89a646de8c2c8f6944c305bd1e6d84374af6ecca
ModerathoWinther <bjorn.mwinther@gmail.com>
2025-05-21 04:27:54 +0200

    [UPDATE]

--- src/main/java/com/pvt/SocialSips/config/FirebaseConfig.java
+++ src/main/java/com/pvt/SocialSips/config/FirebaseConfig.java
@@ -4,17 +4,17 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.*;
+import java.io.IOException;
 
-@Service
+@Component
 public class FirebaseConfig {
 
-    @Value("${firebase.admin.sdk.path}")
-    private File file;
+    @Value("classpath:${firebase.admin.sdk.path}")
+    private Resource resource;
 
     @PostConstruct
     public void onStart() {
@@ -26,15 +26,14 @@ public class FirebaseConfig {
     }
 
     private void initialize() {
-        if (FirebaseApp.getApps().isEmpty() || FirebaseApp.getApps() == null) {
-            try (InputStream inputStream = new DataInputStream(new FileInputStream(file))) {
-                FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(inputStream)).build();
-
-                FirebaseApp.initializeApp(options);
-            } catch (IOException e) {
-                System.out.println(e.getCause() + e.getMessage());
-            }
+        FirebaseOptions options = null;
+        try {
+            options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(resource.getInputStream())).build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        FirebaseApp.initializeApp(options);
+        
     }
 }
 
