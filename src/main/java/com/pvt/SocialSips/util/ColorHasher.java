3ce36e497a1fd33da4d639bf1cbc34b53611af2e
maxi <maxi.ellnestam@gmail.com>
2025-05-24 15:55:48 +0200

    [UPDATE] color

--- src/main/java/com/pvt/SocialSips/util/ColorHasher.java
+++ src/main/java/com/pvt/SocialSips/util/ColorHasher.java
@@ -2,12 +2,16 @@ package com.pvt.SocialSips.util;
 
 public class ColorHasher {
     public static String intToColorHex(int groupNumber, int totalGroups) {
+        if (totalGroups <= 0) {
+            throw new IllegalArgumentException("Total groups must be > 0");
+        }
+
         float hue = (groupNumber * 360.0f / totalGroups) % 360;
 
         float saturation = 0.65f;
-        float lightness = 0.5f;
+        float brightness = 0.80f;
 
-        java.awt.Color color = java.awt.Color.getHSBColor(hue / 360.0f, saturation, lightness);
+        java.awt.Color color = java.awt.Color.getHSBColor(hue / 360f, saturation, brightness);
         return String.format("#%02X%02X%02X", color.getRed(), color.getGreen(), color.getBlue());
     }
 }
