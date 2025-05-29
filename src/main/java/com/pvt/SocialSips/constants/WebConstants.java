7b0235fd9c6a4854dc28617dbdffa223b73f95bb
Nomis778 <sijulemi@gmail.com>
2025-05-29 14:37:48 +0200

    [UPDATE] endpoint permissions

--- src/main/java/com/pvt/SocialSips/constants/WebConstants.java
+++ src/main/java/com/pvt/SocialSips/constants/WebConstants.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 
 public final class WebConstants {
 
-    public static final String[] PROTECTED_ENDPOINTS = {"/user/questpools", "/event", "/event/start", "event/guests", "/questpool/**", "/user"};
+    public static final String[] PROTECTED_ENDPOINTS = {"/user", "/user/questpools", "/event", "/event/start", "/event/guests", "/questpool/**"};
 
-    public static final String[] PERMITTED_ENDPOINTS = {"/", "/home", "/css/**", "/error", "/__/hosting/**",
-            "/event/join/**", "/token", "/.well-known/**",
-            "/h2-console/**","/questpool/standard"};
+    public static final String[] PERMITTED_ENDPOINTS = {"/", "/home", "/css/**", "/error", "/__/hosting/**", "/h2-console/**",  "/.well-known/**",
+            "/event/join/**", "/event/questpools/**", "/event/started/**", "/event/guests/**",
+            "/questpool/standard", "/token"
+    };
 
     public static final String[] ALLOWED_ORIGINS = {"https://social-sips-ec954.web.app",
             "https://social-sips-ec954.firebaseapp.com",
